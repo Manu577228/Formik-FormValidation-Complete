c47ffd036f74a6a935249524cd48dab56e3ca007
Manubharadwaj5772 <84725175+Manubharadwaj5772@users.noreply.github.com>
2022-05-15 18:30:32 +0530

    Formik validation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import Login from './components/Login';
+// import Registration from './components/Registration';
+import EnrollmentForm from './components/EnrollmentForm';
+
+function App() {
+  return (
+    <div className="App">
+   {/* <Login /> */}
+   {/* <Registration /> */}
+   <EnrollmentForm />
+    </div>
+  )
+}
+
+export default App;
